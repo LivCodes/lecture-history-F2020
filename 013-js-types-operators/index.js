5ae2c20268cfddd6bb6571e9d47766b6e8e91947
Ann Duong <thuyanannduong@gmail.com>
2020-10-20 18:02:26 +0000

    added lecture 13

---
+++ 013-js-types-operators/index.js
@@ -0,0 +1,18 @@
+let str = "hello cat ladies";
+console.log(str);
+
+let number = 3;
+console.log(number + 1);
+
+/*
+Primitive
+    boolean
+    number
+    string
+    undefined - default absence of a value 
+    null - explicit absence of a value (on purpose)
+    BigInt - fancy number
+    Symbol - fancy string
+
+    Objects
+*/
