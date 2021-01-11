cdcae16f918fca5dd22b6fa0d2335c455ad67cc3
Ann Duong <thuyanannduong@gmail.com>
2021-01-11 19:48:00 +0000

    added stacks

--- 046-stacks/README.md
+++ 046-stacks/README.md
@@ -4,25 +4,32 @@
 - A stack is an abstraction.
 - It is a list that only allows the user to process elements in a LIFO manner.
 
+## Mental Model and Visualization
+
 ## Important Terminology
 - LIFO (Last In, First Out)
 - push (add element to the "top" of the list)
-- pop (remove elements from the "top" of the list)
-
-## Mental Model and Visualization
+- pop (remove element from the "top" of the list)
 
 ## What can stacks be used to model?
-- 
+- model open and closing pairs 
+- stack of books 
+- going back and forth in our browsing history
+- Pez dispenser
+
 
 ## Stack methods
 - size()
-- isEmpty()
-- push()
-- pop() //throw an error if there is nothing to pop
-- peek()
+- isEmpty() // returns boolean
+- push() // add an item to the "top" of the stack 
+- pop() // remove an item from the "top" of the stack
+- peek() // show us what's at the top 
 
 ## Are there different ways to implement a Stack?
 - Yes, but arrays are simple AND fast!
+
+
+
 
 ## Bounded Stacks 
 - A stack that has a maxSize.
--- 046-stacks/index.js
+++ 046-stacks/index.js
@@ -1,1 +1,43 @@
-console.log("today, we're learning stacks")
+class Stack {
+    constructor(maxSize){
+        this._stack = [];
+        this.maxSize = maxSize;
+    }
+    
+    size(){
+        return this._stack.length;
+    }
+    
+    isEmpty(){
+        return this._stack.length === 0;
+    }
+    
+    push(item){
+        /*if the length is less then or equal to the max size*/
+        if( this.size() < this.maxSize ){
+            this._stack.push(item);
+            return `${item} was pushed on top of the stack`
+        } else {
+            throw new Error("Stack Overflow")
+        }
+    }
+    
+    pop(){
+        if(!this.isEmpty()){
+            let removed = this._stack.pop()
+            return `${removed} was popped from the top of the stack`
+        } else {
+            throw new Error("Stack Underflow")   
+        }
+    }
+    
+    peek(){
+        return this._stack[this._stack.length - 1];
+    }
+}
+
+let books = new Stack(4);
+books.push("Harry Potter");
+books.push("Eragon");
+books.push("Milk and Honey")
+
