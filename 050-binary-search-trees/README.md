72267ce84a30456cff1d51061a0742601e9dbdc6
Ann Duong <thuyanannduong@gmail.com>
2021-01-14 19:16:24 +0000

    added tree

--- 050-binary-search-trees/README.md
+++ 050-binary-search-trees/README.md
@@ -1,19 +1,43 @@
 # Binary Search Trees
+- where ALL left decendants are less that the node 
+- ALL right decents are greater than the node
+- Best when your data source is average spaced out, random
+
+### Let's play a guessing game
+- I'm thinking of an integer from 1 to 10 (inclusive).
+- Your goal is to guest that exact integer in the fewest amount of trys.
+- If you're wrong, I'll tell you in you are too high or too low.
 
 ## Throwback
-* What is the run time for searching for something in a sorted list?
-* What if we want to insert things to a sorted list?
+* What is the run time for searching for something in a sorted array?
+    * O(log n)
+* What if we want to insert things to a sorted array?
+    * O(n)
 
-## Accumulating and searching are so common, can we make it better?
+## Accumulating data and searching through data are so common
+* Can we make it better?
+* Yes! With Binary Search Trees
 
 ## Why are we learning about Binary Search Trees?
 - Interviews
 - They are an alternative to sorted arrays
 - Binary Search Tree have same O(log n) search run-time as arrays 
-- But they have better insertion and deletion time
+- But they have better insertion and deletion time 
+    - ON AVERAGE, O (log n) 
+    - O(h) where h is the average height of the tree
 
 ### Setting up our Binary Search Tree
+- Given this array = [50, 30, 80, 40, 90, 20, 70]
+    - interate through this array 
+    
+    - first node you write, should be 50
+    - then insert 30, 80, 40, 90, then 20
+    - last node you insert, should be 70
+    
 
-### Inserting all elements
+- Given an array = [20, 30, 40, 50, 70, 80, 90]
 
-### Searching elements
+### Inserting all nodes
+- Iterate through the loop
+
+### Searching nodes
--- 050-binary-search-trees/index.js
+++ 050-binary-search-trees/index.js
@@ -1,1 +1,67 @@
-console.log("climb that tree")
+class BinarySearchTreeNode {
+    constructor(value = null){
+        this.value = value;
+        this.left = null; 
+        this.right = null;
+    }
+    
+    find(searchNumber){ //return true if number is in our tree; false otherwise
+        if(!this.value){
+            return false;
+        }
+        if(this.value === searchNumber){
+            return true
+        }
+        if(searchNumber < this.value){
+            if(!this.left){
+                return false;
+            }
+            return this.left.find(searchNumber)
+        }
+        if(searchNumber > this.value){
+            if(!this.right){
+                return false
+            }
+            return this.right.find(searchNumber)
+        }
+        
+    }
+    
+    insert(number){ //assume that number is a integer
+        if(!this.value){
+            this.value = number;
+        } else {
+            if(number < this.value){
+                if(!this.left){
+                    this.left = new BinarySearchTreeNode(number)
+                } else {
+                    this.left.insert(number)
+                }
+            }else if(number > this.value){
+                if(!this.right){
+                    this.right = new BinarySearchTreeNode(number)
+                } else {
+                    this.right.insert(number)
+                }
+            }else{
+                throw new Error("Cannot add duplicate to the tree")
+            }
+        }
+    }
+}
+
+function print(node){
+    if(node){
+        print(node.left);
+        console.log(node.value);
+        print(node.right);
+    }
+}
+
+let array = [50, 30, 80, 40, 90, 20, 70];
+
+let tree = new BinarySearchTreeNode();
+for(const number of array){
+    tree.insert(number)
+}
+
