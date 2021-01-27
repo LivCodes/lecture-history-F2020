457ff2d10e53444b63fb8093fa255009464272b1
Ann Duong <thuyanannduong@gmail.com>
2021-01-27 14:24:44 -0500

    added events code

--- 055-events/README.md
+++ 055-events/README.md
@@ -4,25 +4,37 @@
 
 ## What types of events are there?
 * https://developer.mozilla.org/en-US/docs/Web/Events
-
+- keypress 
+- mouseenter and mouseleave
+- click 
+- doubleclick
 
 * Very important one: DOMContentLoaded
+- IF you have code that depends on HTML elements being loaded, put it in the DOMContentLoaded
 
-## .addEventListener
+## element.addEventListener(type, callback)
 - takes in a string for the name of the event, and a callback function to be invoked when the event happens
 - (optional) argument for the callback is the event itself
 
 ## Review Callbacks!
 - A callback is a function that is passed as an argument into another function
+- .map(func)
 
 ## Practice
 1. Refactor our code to generate our table of instructors based on a given array of data.
-2. When I click on an ".email" <td>, the color changes to blue
-3. When I click on a row, the background color changes to gray
+2. When I click on an "thead" <td>, the text color changes to red.
+3. When I click on an "email" <td>, the text color changes to blue.
+4. When I click on a row, the selected instructor appears in a div.
+
+
+## Where do I put .addEventListener()
+- Waited until the DOM is loaded, in that function, we add eventlisteners
+- addEventListen WHEN you created that elements
 
 
 ## The event object
-- `event.target` vs `event.currentTarget`
+- `event.target` : the exact element that was clicked (triggered event)
+`event.currentTarget` : the element that the event listener is attached to
 
 ## Bubbling vs. Capturing, and Propagation
 - Capturing phase – the event goes down to the element.
--- 055-events/index.js
+++ 055-events/index.js
@@ -1,21 +1,82 @@
+const teachers = [
+  {
+    firstName: "Ann", 
+    lastName: "Duong", 
+    email: "ann@gmail.com"
+  },
+  {
+    firstName: "Reuben", 
+    lastName: "Ogbonna", 
+    email: "reuben@gmail.com"
+  },
+  {
+    firstName: "Erwins", 
+    lastName: "Saget", 
+    email: "erwins@gmail.com"
+  },
+]
+
+// User stories: 
+// As a user, I can click on the table header to make it red
+// As a user, I can click on a row to make it the selected instructor
+document.addEventListener("DOMContentLoaded", () => {
+  let header = document.querySelector("table")
+  header.addEventListener("click", clickBait )
+
+  let thead = document.querySelector("thead")
+  thead.addEventListener("click", handleTableHeader)
+  
+  teachers.forEach(teach => {
+    addInstructor(teach.firstName, teach.lastName, teach.email);
+  })
+})
+
+function handleTableHeader(e){
+  console.log(e.target)
+  e.target.style.color = "red"
+}
+
+function handleEmailClick(e){
+  e.stopPropagation();
+  console.log("updating the email td")
+  e.target.style.color = "blue"
+}
+
+function clickBait(){
+  console.log("I have clicked the table")
+}
+
+function selectInstructor(e){
+  e.stopPropagation();
+  console.log("change the selected instructor tr")
+  let row = e.currentTarget;
+  document.querySelector("span").innerText = row.innerText
+}
 
 function addInstructor(firstName, lastName, email){
-  // create a new tr and adding it to tbody
-  const trEl = document.createElement("tr");
+  //Here's the row
+  const rowElement = document.createElement("tr");
+  rowElement.classList.add("instructor")
+
+  rowElement.addEventListener("click", selectInstructor)
+
   const tbody = document.getElementById("table-body");
-  tbody.appendChild(trEl);
-
-  //creating 3 td and adding them to tr
-  const td1 = document.createElement("td");
-  const td2 = document.createElement("td");
-  const td3 = document.createElement("td");
-  trEl.append(td1, td2, td3)
-
-  //update the text inside the element
-  td1.innerText = firstName;
-  td2.innerText = lastName;
-  td3.innerText = email;
+  tbody.appendChild(rowElement);
+  const firstNameEl = document.createElement("td");
+  const lastNameEl = document.createElement("td");
+  const emailElement = document.createElement("td");
+  rowElement.append(firstNameEl, lastNameEl, emailElement)
+  firstNameEl.innerText = firstName;
+  lastNameEl.innerText = lastName;
+  emailElement.innerText = email;
+  emailElement.classList.add("email");
+  emailElement.addEventListener("click", handleEmailClick)
 }
 
 
-//parentNode.removeChild()
+
+/*function addEventListener(type, callback){}
+if type is a click 
+else if type is a keypress 
+
+*/
