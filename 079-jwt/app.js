c596cf7ec181e97819ee43f638df8afb7ab80f8d
Ann Duong <thuyanannduong@gmail.com>
2021-04-07 10:01:18 -0400

    prelecture

--- 079-jwt/app.js
+++ 079-jwt/app.js
@@ -6,19 +6,27 @@ const usersRouter = require('./routes/usersRouter')
 const petsRouter = require('./routes/petsRouter')
 const User = require("./models/User")
 
-// Allows us to accept form requests and AJAX requests
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// Logging middleware
-app.use("/", (req, res, next) => {
-  console.log(req.method, req.path);
-  next();
+// Create a user and store their info and encrypted password in DB
+app.post('/users', async (req, res) => {
+  const {name, email, password} = req.body;
+  try {
+    if (name && email && password){
+      const saltRounds = 10;
+      const encrypted_password = await bcrypt.hash(password, saltRounds);
+      const user = await User.createUser(name, email, encrypted_password)
+      res.status(201).json(user)
+    } else {
+      res.status(400).json({message: "Bad request. Check the body."})
+    }
+  } catch {
+    res.sendStatus(500)
+  }
 })
 
-app.use('/users', usersRouter)
-app.use('/pets', petsRouter)
-
+// Checks a request to make sure the email and passwords match
 app.post('/login', async (req, res) => {
   const {email, password} = req.body;
   const user = await User.getUserByEmail(email);
@@ -34,5 +42,8 @@ app.post('/login', async (req, res) => {
     res.status(401).json({message: "These invalid credentials."})
   }
 })
+
+app.use('/users', usersRouter)
+app.use('/pets', petsRouter)
 
 app.listen(PORT, () => {console.log(`Server starting on port ${PORT}`)});
--- 079-jwt/models/User.js
+++ 079-jwt/models/User.js
@@ -1,8 +1,6 @@
 const db = require('../db/config')
 
 class User {
-  //responsible for communicating with our DB
-
   static getAllUsers(){
     const queryText = "SELECT * FROM users ORDER BY id";
     return db.query(queryText).then(results => results.rows)
